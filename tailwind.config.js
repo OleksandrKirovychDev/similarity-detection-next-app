02d16c83e8150e9edce15103d222a3fd1c582e69
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2023-04-09 19:36:55 +0300

    tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+const { colors } = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +13,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+      colors: {
+        ...colors,
+        "light-gols": "#f5bc51",
+        "dark-gold": "#533519",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        "2xl": "1360px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate", require("@tailwindcss/typography"))],
 };
