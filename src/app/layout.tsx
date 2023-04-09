ffe3592aab36ccec6dff98137d6d3b1e797b188c
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2023-04-09 19:40:01 +0300

    app - layout, navbar and toaster added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/ui/Toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,11 @@ export default function RootLayout({
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
           {children}
-          {/* <Navbar /> */}
+
+          <Toaster position="bottom-right" />
+
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
         </Providers>
 
         <div className="h-49 md:hidden"></div>
